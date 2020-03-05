810fb31e0c118fc9835136a57d7c7c844cf633a7
Tal Gao <liutao.gao@gmail.com>
2020-03-05 14:03:33 +0200

    formate update

--- todolist/src/components/TodoTable.js
+++ todolist/src/components/TodoTable.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Todolist from './Todolist';
-export default function TodoTable (props){
-    return(
+export default function TodoTable(props) {
+    return (
         <div>
             <table>
-                    <tbody>
-                        {
-                            props.todos.map((item,index) => 
-                                <tr key={index}>
-                                    <td>{item.desc}</td>
-                                    <td>{item.date}</td>
-                                    <td><button onClick={() => props.deleteItem(index)} >Delete</button></td>
-                                    <td><button onClick={() => props.changeDesc(index)}>Change Desc</button></td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+                <tbody>
+                    {
+                        props.todos.map((item, index) =>
+                            <tr key={index}>
+                                <td>{item.desc}</td>
+                                <td>{item.date}</td>
+                                <td><button onClick={() => props.deleteItem(index)} >Delete</button></td>
+                                <td><button onClick={() => props.changeDesc(index)}>Change Desc</button></td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
 
 
         </div>
--- todolist/src/components/Todolist.js
+++ todolist/src/components/Todolist.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TodoTable from './TodoTable';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
-const Todolist = () => { 
-    const [todo, setTodo] = useState({desc:'', date:''});
+const Todolist = () => {
+    const [todo, setTodo] = useState({ desc: '', date: '' });
     const [todos, setTodos] = useState([]);
 
     const inputChanged = (event) => {
-        setTodo({...todo, [event.target.name] : event.target.value});
+        setTodo({ ...todo, [event.target.name]: event.target.value });
     };
 
     const addTodo = (event) => {
-        
-        setTodos([{desc: todo.desc, date : todo.date}, ...todos]);
+
+        setTodos([{ desc: todo.desc, date: todo.date }, ...todos]);
     };
 
     const deleteItem = (row) => {
@@ -22,27 +22,27 @@ const Todolist = () => {
 
 
 
-    const columns =[
+    const columns = [
         {
-            Header:'Description',
-            accessor:'desc',
+            Header: 'Description',
+            accessor: 'desc',
         },
         {
-            Header:'Date',
-            accessor:'date',
+            Header: 'Date',
+            accessor: 'date',
         },
         {
-            Cell: row => <button onClick={()=>deleteItem(row.index)}>Delete</button>
+            Cell: row => <button onClick={() => deleteItem(row.index)}>Delete</button>
         }
-      
+
     ]
-    return(
+    return (
         <div className="inputHolder">
             Desc: <input type="text" onChange={inputChanged} value={todo.desc} name="desc" />
-            Date: <input type="date" value = {todo.date} name="date" onChange={inputChanged} />
+            Date: <input type="date" value={todo.date} name="date" onChange={inputChanged} />
             <button onClick={addTodo}>Add</button>
-          
-            <ReactTable data ={todos} columns={columns} defaultPageSize={10} filterable={true}></ReactTable>
+
+            <ReactTable data={todos} columns={columns} defaultPageSize={10} filterable={true}></ReactTable>
         </div>
     );
 }
