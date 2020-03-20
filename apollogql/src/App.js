72a88a3b3f9bac6e4fbfad2e43449f67d06cfcbd
SharkTal <liutao.gao@gmail.com>
2020-03-20 12:30:32 +0200

    GraphQL

--- apollogql/src/App.js
+++ apollogql/src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
 
 function App() {
+  const jobquery = gql`
+    {
+      jobs {
+        title,
+        company{name},
+        applyUrl,
+      }
+    }`;
+
+  const { loading, error, data } = useQuery(jobquery);
+
+  if (loading)
+    return "Loading...";
+  if (error)
+    return `Error ${error.message}`;
+
+  const columns = [
+    {
+      Header: 'Title',
+      accessor: 'title',
+      id: 0
+    },
+    {
+      Header: 'Company Name',
+      accessor: d => d.company.name,
+      id: 1
+    },
+    {
+      Header: 'Apply Url',
+      accessor: 'applyUrl',
+      id: 2
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactTable
+        data={data.jobs}
+        columns={columns}
+        defaultPageSize={15}
+        filterable={true}
+      />
     </div>
   );
 }
